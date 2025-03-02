504401d09dbdcbc426c9f1906ecd6493812a44e3
Chetan Ranpariya <ranpariyachetan@users.noreply.github.com>
2025-03-02 12:31:00 +0800

    Using LangChain Message Types in chatbot.

--- chatbot.py
+++ chatbot.py
@@ -1,22 +1,23 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import  load_dotenv
-from langchain_core.prompts import  PromptTemplate, load_prompt
-
 
 load_dotenv()
 
 model = ChatOpenAI(model="gpt-4")
 
-chat_history = []
+chat_history = [
+    SystemMessage(content="You are a helpful AI assistant.")
+]
 
 while True:
     user_input = input("You: ")
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
     if user_input == "exit":
         break
 
     result = model.invoke(chat_history)
-    chat_history.append(result.content)
+    chat_history.append(AIMessage(content=result.content))
     print(f"AI: {result.content}")
 
 print(chat_history)
