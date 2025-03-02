2c163bf8528974d14ccfcdc9a5f63cfe52724890
Chetan Ranpariya <ranpariyachetan@users.noreply.github.com>
2025-03-02 21:27:08 +0800

    Used text file to load message history into MessagePlaceholder.

--- chat_prompt_template.py
+++ chat_prompt_template.py
@@ -1,10 +1,9 @@
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 
-chat_template = ChatPromptTemplate.from_messages([
+chat_template = ChatPromptTemplate([
     ("system", "You are a helpful {domain} expert."),
     ("human", "Explain in simple terms, what is {topic}.")
 ])
