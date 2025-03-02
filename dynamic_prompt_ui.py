0b2e12b46eb799f65231a2e0b3823355f118ebaa
Chetan Ranpariya <ranpariyachetan@users.noreply.github.com>
2025-03-02 11:57:09 +0800

    Added code for creating and re-using templates as json.

--- dynamic_prompt_ui.py
+++ dynamic_prompt_ui.py
@@ -1,8 +1,6 @@
-from click import style
-from langchain.chains.qa_with_sources.stuff_prompt import template
 from langchain_openai import ChatOpenAI
 from dotenv import  load_dotenv
-from langchain_core.prompts import  PromptTemplate
+from langchain_core.prompts import  PromptTemplate, load_prompt
 
 import streamlit as st
 
@@ -17,22 +15,7 @@ style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "T
 
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
-# template = load_prompt('template.json')
-template = PromptTemplate(
-    template = f"""
-        Please summarize the research paper titles "{paper_input}" with the following specifications:
-        Explanation Style: {style_input}
-        Explanation Length: {length_input}
-        1. Mathematical Details:
-            - Include relevant mathematical equations if present in the paper.
-            - Explain the mathematical concepts using simple, intuitive code snippets where applicable.
-        2. Analogies:
-            - Use relatable analogies to simplify complex ideas.
-        If certain information is not available in the paper, respond with "Insufficient information available" instead of guessing.
-        Ensure the summary is clear, accurate and aligned with the provided style and length requirements.
-    """,
-    input_variables = ["paper_input", "style_input", "length_input"]
-)
+template = load_prompt("template.json")
 
 # fill the placeholders.
 prompt = template.invoke({
